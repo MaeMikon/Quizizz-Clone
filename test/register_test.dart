96ebac0269474dcdc1b813f43e751c9cf70b9897
MaeMikon <39037356+MaeMikon@users.noreply.github.com>
2021-04-22 08:55:25 +0700

    Update register_test.dart

--- test/register_test.dart
+++ test/register_test.dart
@@ -1,9 +1,7 @@
-mport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-
 import 'package:quiz_app/screens/register/register_screen.dart';
 
 void main() {
@@ -13,16 +11,17 @@ void main() {
       final password = find.byKey(ValueKey("Password"));
       final email = find.byKey(ValueKey("Email"));
       final confirmPassword = find.byKey(ValueKey("ConfirmPassword"));
+      final sampleUser = "Linh Linh@gmail.com 541999";
       final createButton = find.byKey(ValueKey("registerButton"));
     //excecute Test
       await tester.pumpWidget(MaterialApp(home: Register_Screen(),));
       await tester.enterText(username, "Linh");
       await tester.enterText(email, "Linh@gmail.com");
       await tester.enterText(password, "541999");
-      await tester.enterText(confirmPassword, "5419flu99");
+      await tester.enterText(confirmPassword, "541999");
       await tester.tap(createButton);
     //expectedOutput
-      expect(Register_Screen.register_result,true);
+      expect(Register_Screen.isCreated,true);
 
   });
 }
